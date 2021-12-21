f38fe2d1acbac292beef3c67fc934c7f66d134ea
Nathan Mundis - C <nmundis@blueorigin.com>
2021-12-21 13:39:48 -0800

    new message

--- application.py
+++ application.py
@@ -3,4 +3,5 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hi Jaimie, Richie, and Bobby!  How are you today?'
+    return "Oh, so you were looking to run a report? Well, I can't do that but I can ask how you are doing today! How are you?"
+
