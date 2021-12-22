d37030d19d0b9341c929974f451053c3e9e9f297
Nathan Mundis - C <nmundis@blueorigin.com>
2021-12-21 16:32:00 -0800

    Now I output a figure the code generates

---
+++ MyWaves.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
+
+class MyWaves:
+    def __init__(self, NumHarmonics = 5):
+        self.NumPts =NumPts = 100000
+        self.Periods = Periods = 3
+        self.w = w = 1.0e3  # hertz
+
+        self.tLength = tLength = Periods/w  # seconds
+        self.t = np.linspace(0, tLength, NumPts)
+        self.NumHarmonics = NumHarmonics
+        self.PeriodsToShow = Periods
+        pass
+
+    def SquareWave(self):
+        self.PSq = PSq = np.zeros_like(self.t)
+        PSq[::] = np.sin(2*np.pi*self.w*self.t)
+        for jj, p in enumerate(PSq):
+            if p >= 0.0:
+                PSq[jj] = 1.0
+            else:
+                PSq[jj]= -1.0
+        pass
+
+    def ApproxSqWave(self):
+        self.PASq = PASq = np.zeros_like(self.t)
+        for ii in range(1,2*self.NumHarmonics+1,2):
+            PASq += 4/np.pi*np.sin(2*np.pi*self.w*ii*self.t)/ii
+        pass
+
+
+    def PlotWaves(self):
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.plot(self.t, self.PSq)
+        axis.plot(self.t, self.PASq)
+        axis.set_xlim((0, self.PeriodsToShow/self.w))
+        axis.grid()
+        fig.tight_layout()
+        output = io.BytesIO()
+        FigureCanvas(fig).print_png(output)
+        return output
--- application.py
+++ application.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, make_response, Response
+from MyWaves import MyWaves
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "Oh, so you were looking to run a report? Well, I can't do that but I can ask how you are doing today! How are you?"
+    OhHi = "Oh, so you were looking to run a report? Well, I can't do that but I can ask how you are doing today! How are you?"
+    MkWv = "Also, let's make waves!"
+    wave = MyWaves(3)
+    wave.SquareWave()
+    wave.ApproxSqWave()
+    FigOut = wave.PlotWaves()
+    DnWv = "Now those are some waves!"
+    NewLn = "\n"
+    output = f"{OhHi}{NewLn}{NewLn}{MkWv}{NewLn}{NewLn}{DnWv}{NewLn}"
+    print(output)
+    #response = make_response(output.encode())
+    #response.mimetype = "text/plain"
+    return Response(FigOut.getvalue(), mimetype='image/png')
 
